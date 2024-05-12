7d57de56fc5d654152af66ce7c8e31235ac59b30
Godwin Acheampong <21052794@student.hhs.nl>
2024-05-12 19:40:19 +0200

    pytorch gebruikt CPU i.p.v. CUDA

--- presentatie_demo/obj_detection.py
+++ presentatie_demo/obj_detection.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 
-import cv2
+# import cv2
 
 model = YOLO("yolov8s.pt")
 
 results = model.predict(source="0", show=True)
 
 print(results)
+
+
+# pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cpu
